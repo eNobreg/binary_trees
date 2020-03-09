2ce7daf1ea48a760c8b34d25905334598882f3cc
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-09 13:07:55 -0400

    adding add node and updated header with prototype

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * *binary_tree_node - function that creates a node
+ * parent: pointer to parent node
+ * value: value to be stored in node
+ * Return: pointer or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *TempNode = malloc(sizeof(binary_tree_t));
+	TempNode->n = value;
+	TempNode->*parent = parent;
+	TempNode->*left = NULL;
+	TempNode->*right = NULL;
+
+	return (*TempNode);
+}
