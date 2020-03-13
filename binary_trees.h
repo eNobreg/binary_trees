98d56897b36e01ca3ef63a5c9eb9de36c2fc1509
Jeremy Bedolla <jjbedolla@gmail.com>
2020-03-13 14:11:28 -0400

    [Update] added advanced func prototype

--- binary_trees.h
+++ binary_trees.h
@@ -48,7 +48,7 @@ int is_power(int n);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int binary_tree_is_bst(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
